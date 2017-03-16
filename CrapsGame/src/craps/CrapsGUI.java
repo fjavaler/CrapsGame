54dac46808d3ed0b4e642e781ec6676e0fcca891
fjavaler <conscientiadivinus@gmail.com>
2017-03-16 15:27:49 -0600

    Added dice and questionMark png files

--- CrapsGame/src/craps/CrapsGUI.java
+++ CrapsGame/src/craps/CrapsGUI.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingConstants;
+
 import java.awt.Font;
 import javax.swing.JMenu;
 import javax.swing.JLabel;
@@ -66,7 +67,7 @@ public class CrapsGUI
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmSave = new JMenuItem("Save");
+		JMenuItem mntmSave = new JMenuItem("New Shooter");
 		mnFile.add(mntmSave);
 		
 		JMenuItem mntmExit = new JMenuItem("Exit");
@@ -75,15 +76,55 @@ public class CrapsGUI
 		JMenu mnGameStats = new JMenu("Game Stats");
 		menuBar.add(mnGameStats);
 		
+		JMenuItem mntmTotalGames = new JMenuItem("Total Games");
+		mnGameStats.add(mntmTotalGames);
+		
+		JMenuItem mntmTotalWins = new JMenuItem("Total Wins");
+		mnGameStats.add(mntmTotalWins);
+		
+		JMenuItem mntmAvgRollsgame = new JMenuItem("Avg Rolls/Game");
+		mnGameStats.add(mntmAvgRollsgame);
+		
+		JMenuItem mntmSummary = new JMenuItem("Summary");
+		mnGameStats.add(mntmSummary);
+		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
+		
+		JMenuItem mntmInstructions = new JMenuItem("Instructions");
+		mnHelp.add(mntmInstructions);
 		
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mnHelp.add(mntmAbout);
 		frmCrapsAGame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(CrapsGUI.class.getResource("/dice.png")));
 		lblNewLabel.setBounds(6, 6, 240, 142);
 		frmCrapsAGame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblDie = new JLabel("Die 1");
+		lblDie.setBounds(63, 169, 61, 16);
+		frmCrapsAGame.getContentPane().add(lblDie);
+		
+		JLabel lblDie_1 = new JLabel("Die 2");
+		lblDie_1.setBounds(136, 169, 61, 16);
+		frmCrapsAGame.getContentPane().add(lblDie_1);
+		
+		JLabel lblSum = new JLabel("Sum");
+		lblSum.setBounds(209, 169, 61, 16);
+		frmCrapsAGame.getContentPane().add(lblSum);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(50, 197, 60, 60);
+		frmCrapsAGame.getContentPane().add(label);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setBounds(123, 197, 60, 60);
+		frmCrapsAGame.getContentPane().add(label_1);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setBounds(196, 197, 60, 60);
+		frmCrapsAGame.getContentPane().add(label_2);
 	}
 }
