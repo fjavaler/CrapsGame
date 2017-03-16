0c54d8b92e9892dfa7fd0bf174a8af56785ce350
fjavaler <conscientiadivinus@gmail.com>
2017-03-16 14:56:00 -0600

    Added Dice image and menubar

--- CrapsGame/src/craps/CrapsGUI.java
+++ CrapsGame/src/craps/CrapsGUI.java
@@ -6,11 +6,17 @@ package craps;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JMenu;
+import javax.swing.JLabel;
 
 public class CrapsGUI
 {
 
-	private JFrame frame;
+	private JFrame frmCrapsAGame;
 
 	/**
 	 * Launch the application.
@@ -24,7 +30,7 @@ public class CrapsGUI
 				try
 				{
 					CrapsGUI window = new CrapsGUI();
-					window.frame.setVisible(true);
+					window.frmCrapsAGame.setVisible(true);
 				}
 				catch (Exception e)
 				{
@@ -47,9 +53,36 @@ public class CrapsGUI
 	 */
 	private void initialize()
 	{
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCrapsAGame = new JFrame();
+		frmCrapsAGame.setTitle("Craps - A Game of Chance and Skill!");
+		frmCrapsAGame.setBounds(100, 100, 750, 400);
+		frmCrapsAGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(new Font("Noto Sans UI", Font.PLAIN, 14));
+		frmCrapsAGame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnFile.add(mntmSave);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnGameStats = new JMenu("Game Stats");
+		menuBar.add(mnGameStats);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		frmCrapsAGame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(18, 6, 223, 129);
+		frmCrapsAGame.getContentPane().add(lblNewLabel);
 	}
-
 }
