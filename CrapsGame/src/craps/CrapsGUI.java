1cb4838f03822ff8ab3cea02cb0a43bc90ab68f2
fjavaler <conscientiadivinus@gmail.com>
2017-03-17 13:56:07 -0600

    Added Craps.java, CrapsGame.java, and Die.java

--- CrapsGame/src/craps/CrapsGUI.java
+++ CrapsGame/src/craps/CrapsGUI.java
@@ -91,101 +91,115 @@ public class CrapsGUI
 		frmCrapsAGame.setBounds(100, 100, 750, 400);
 		frmCrapsAGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCrapsAGame.setLocationRelativeTo(null);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setFont(new Font("Noto Sans UI", Font.PLAIN, 14));
 		frmCrapsAGame.setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmSave = new JMenuItem("New Shooter");
 		mnFile.add(mntmSave);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mnFile.add(mntmExit);
-		
+
 		JMenu mnGameStats = new JMenu("Game Stats");
 		menuBar.add(mnGameStats);
-		
+
 		JMenuItem mntmTotalGames = new JMenuItem("Total Games");
 		mnGameStats.add(mntmTotalGames);
-		
+
 		JMenuItem mntmTotalWins = new JMenuItem("Total Wins");
 		mnGameStats.add(mntmTotalWins);
-		
+
 		JMenuItem mntmAvgRollsgame = new JMenuItem("Avg Rolls/Game");
 		mnGameStats.add(mntmAvgRollsgame);
-		
+
 		JMenuItem mntmSummary = new JMenuItem("Summary");
 		mnGameStats.add(mntmSummary);
-		
+
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
-		
+
 		JMenuItem mntmInstructions = new JMenuItem("Instructions");
 		mnHelp.add(mntmInstructions);
-		
+
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mnHelp.add(mntmAbout);
 		frmCrapsAGame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(CrapsGUI.class.getResource("/dice.png")));
 		lblNewLabel.setBounds(6, 6, 240, 142);
 		frmCrapsAGame.getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblDie = new JLabel("Die 1");
 		lblDie.setFont(new Font("Noto Sans UI", Font.BOLD, 15));
 		lblDie.setBounds(63, 169, 61, 16);
 		frmCrapsAGame.getContentPane().add(lblDie);
-		
+
 		JLabel lblDie_1 = new JLabel("Die 2");
 		lblDie_1.setFont(new Font("Noto Sans UI", Font.BOLD, 15));
 		lblDie_1.setBounds(136, 169, 61, 16);
 		frmCrapsAGame.getContentPane().add(lblDie_1);
-		
+
 		JLabel lblSum = new JLabel("Sum");
 		lblSum.setFont(new Font("Noto Sans UI", Font.BOLD, 15));
 		lblSum.setBounds(209, 169, 61, 16);
 		frmCrapsAGame.getContentPane().add(lblSum);
-		
+
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(CrapsGUI.class.getResource("/questionMark.png")));
 		label.setBounds(50, 197, 60, 60);
 		frmCrapsAGame.getContentPane().add(label);
-		
+
 		JLabel label_1 = new JLabel("");
 		label_1.setIcon(new ImageIcon(CrapsGUI.class.getResource("/questionMark.png")));
 		label_1.setBounds(123, 197, 60, 60);
 		frmCrapsAGame.getContentPane().add(label_1);
-		
+
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon(CrapsGUI.class.getResource("/questionMark.png")));
 		label_2.setBounds(196, 197, 60, 60);
 		frmCrapsAGame.getContentPane().add(label_2);
-		
-//		String name = JOptionPane.showInputDialog(frmCrapsAGame, "Please enter your name:",
-//				"Shooter's Name", JOptionPane.QUESTION_MESSAGE);
-//		
-//		JLabel lblNewLabel_1 = new JLabel(name);
-		JLabel lblNewLabel_1 = new JLabel("Shooter"); //temp
+
+		// String name = JOptionPane.showInputDialog(frmCrapsAGame, "Please
+		// enter your name:",
+		// "Shooter's Name", JOptionPane.QUESTION_MESSAGE);
+		//
+		// JLabel lblNewLabel_1 = new JLabel(name);
+		JLabel lblNewLabel_1 = new JLabel("Shooter"); // temp
 		lblNewLabel_1.setForeground(Color.BLUE);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Noto Sans UI", Font.BOLD, 54));
 		lblNewLabel_1.setBounds(336, 38, 362, 110);
 		frmCrapsAGame.getContentPane().add(lblNewLabel_1);
-		
+
 		JButton btnComeOutRoll = new JButton("Come Out Roll");
-		btnComeOutRoll.addActionListener(new ActionListener() 
+		btnComeOutRoll.addActionListener(new ActionListener()
 		{
-			public void actionPerformed(ActionEvent e) 
+			public void actionPerformed(ActionEvent e)
 			{
-				
+
 			}
 		});
 		btnComeOutRoll.setFont(new Font("Noto Sans UI", Font.BOLD, 15));
 		btnComeOutRoll.setBounds(445, 156, 150, 44);
 		frmCrapsAGame.getContentPane().add(btnComeOutRoll);
+
+		JLabel lblGameStatus = new JLabel("Game Status:");
+		lblGameStatus.setFont(new Font("Noto Sans UI", Font.PLAIN, 13));
+		lblGameStatus.setBounds(50, 299, 89, 16);
+		frmCrapsAGame.getContentPane().add(lblGameStatus);
+
+		JLabel gameStatusTxt = new JLabel("Continue");
+		gameStatusTxt.setForeground(Color.RED);
+		gameStatusTxt.setHorizontalAlignment(SwingConstants.CENTER);
+		gameStatusTxt.setFont(new Font("Noto Sans UI", Font.PLAIN, 18));
+		gameStatusTxt.setBackground(Color.WHITE);
+		gameStatusTxt.setBounds(136, 293, 78, 24);
+		frmCrapsAGame.getContentPane().add(gameStatusTxt);
 	}
 }
